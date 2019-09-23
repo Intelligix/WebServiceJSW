fc9ab17123b77b9644143fdaaa2a4e335456d6a8
Sandro Welter <sandro.welter@intelligix.com.br>
2019-09-23 16:33:33 -0300

    Novo Projeto

---
+++ src/main/java/br/com/intelligix/WebServiceJSW/WebServiceJswApplication.java
@@ -0,0 +1,13 @@
+package br.com.intelligix.WebServiceJSW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceJswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceJswApplication.class, args);
+	}
+
+}
